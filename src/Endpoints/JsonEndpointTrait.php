39be69d5de560795de752fd3cd5d28deacf9cf20
John P. Bloch <johnpbloch@users.noreply.github.com>
2023-07-12 00:11:15 -0500

    Add a json endpoint trait

---
+++ src/Endpoints/JsonEndpointTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JohnPBloch\FluentApi\Endpoints;
+
+use Psr\Http\Message\StreamInterface;
+
+trait JsonEndpointTrait
+{
+    protected function mergeRequestConfigHeaders(): array
+    {
+        return [
+            'Accept' => 'application/json'
+        ];
+    }
+
+    protected function setRequestConfigBody(): mixed
+    {
+        $body = $this->body ?? null;
+        return match (true) {
+            $body === null, $body instanceof StreamInterface, is_resource($body), is_string($body) => $body,
+            default => json_encode($body),
+        };
+    }
+}
